5c45aaef149fc41245798b414f4038e8491f1b1f
dbshinba <okayshuvo@gmail.com>
2025-11-25 20:07:31 +0600

    Update Jobs.js

--- netlify/functions/Jobs.js
+++ netlify/functions/Jobs.js
@@ -2,19 +2,11 @@ exports.handler = async (event) => {
   const jobs = [
     {
       id: 1,
-      title: "ইউটিউব ভিডিও দেখুন",
-      description: "৫ মিনিটের একটি প্রোমো ভিডিও দেখুন এবং কমেন্ট করুন",
-      price: "৫/-",
-      type: "ভিডিও",
-      duration: "05:00"
-    },
-    {
-      id: 2,
-      title: "ফেসবুক পোস্ট লাইক করুন", 
-      description: "ফেসবুক পেজের লেটেস্ট পোস্ট লাইক করুন",
-      price: "৩/-",
-      type: "সোশ্যাল",
-      duration: "05:00"
+      title: "YouTube Video",
+      description: "Watch 5 minute video",
+      price: "5",
+      type: "video", 
+      duration: "05:05"
     }
   ];
   
