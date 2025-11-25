aa56ea0846316058080615c2d755259572fcc7e7
dbshinba <okayshuvo@gmail.com>
2025-11-25 20:16:29 +0600

    Update Jobs.js

--- netlify/functions/Jobs.js
+++ netlify/functions/Jobs.js
@@ -1,17 +1,6 @@
 exports.handler = async (event) => {
-  const jobs = [
-    {
-      id: 1,
-      title: "YouTube Video",
-      description: "Watch 5 minute video",
-      price: "5",
-      type: "video", 
-      duration: "05:05"
-    }
-  ];
-  
   return {
     statusCode: 200,
-    body: JSON.stringify({ jobs: jobs })
+    body: JSON.stringify({ test: "working" })
   };
 };
